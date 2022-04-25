aac1914fafcfb9be10a87dc5482f137ab5b2f355
Alice Hsing <alicehsing@gmail.com>
2022-04-25 16:10:01 -0700

    wrote test to ensure the header is rendering header image and profile name and passed test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
+import Header from './components/layout/Header';
+import App from './App';
 
-test('Should render the header', () => {
+const user = {
+  id: 1,
+  created_at: '2021-12-13T00:17:29+00:00',
+  name: 'Vonta',
+  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+  motto: 'Res Non Verba',
+  color: 'crimson',
+};
 
-})
+describe('header', () => {
+  it('Should render the header', async () => {
+    render(
+        <MemoryRouter>
+          <App />)
+        </MemoryRouter>
+    );
+
+    // Find  and check an image with an alt="Alchemy Logo"
+    const img = await screen.getByAltText('Alchemy Logo');
+    expect(img).toBeInTheDocument();
+    
+    // Find and check the profile name
+    const profileName = await screen.findByText('Vonta');
+    expect(profileName.textContent).toEqual('Vonta');
+  });
+});
+
